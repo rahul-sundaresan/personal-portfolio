b89b596b85dc226e972bec273de8079cc26cf9b1
Rahul Sundaresan <s.rahul@protonmail.com>
2020-02-28 13:19:32 -0500

    added build folder

---
+++ build/precache-manifest.f7374ef1467c733dc6e07fe3b974330e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dbc222af3a03ec9d1c91dd83fdf2e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9632991fb697c724cf4",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "dff8edad0360e4a2dd62",
+    "url": "/static/js/2.cdd62166.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cdd62166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9632991fb697c724cf4",
+    "url": "/static/js/main.add9e8a7.chunk.js"
+  },
+  {
+    "revision": "35a7d5edf96ead885ce4",
+    "url": "/static/js/runtime-main.0eba39f6.js"
+  }
+]);
