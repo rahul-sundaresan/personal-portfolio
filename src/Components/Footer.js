92498fd9552fc5e14daaea0de4a3a0acb03c6716
Rahul Sundaresan <s.rahul@protonmail.com>
2020-02-28 16:27:44 -0500

    Link to github repo of theme creator

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>Thanks to Tim Baker for their <a title="React JS Resume Web App Template" href="https://github.com/tbakerx/react-resume-template">theme</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
