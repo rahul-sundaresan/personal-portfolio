9cd5ea9694223457b8e66e7ee9f460ca74059bb5
Rahul Sundaresan <s.rahul@protonmail.com>
2020-03-24 16:06:59 -0400

    Convert class modules to use hooks

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Footer extends Component {
-  render() {
+export default function Footer({data}) {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+  if(data){
+    var networks= data.social.map(
+      (network) => <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+    )
+  }
 
-    return (
-      <footer>
-
-     <div className="row">
+  return (
+    <footer>
+      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
+            <ul className="social-links"> {networks} </ul>
 
-           <ul className="copyright">
+            <ul className="copyright">
               <li>Thanks to Tim Baker for their <a title="React JS Resume Web App Template" href="https://github.com/tbakerx/react-resume-template">theme</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
               <li>Image by <a href="https://pixabay.com/users/rauschenberger-4614580/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3806668">RENE RAUSCHENBERGER</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3806668">Pixabay</a></li>
-           </ul>
+            </ul>
 
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
-    );
-  }
-}
+      </div>
+    </footer>
+  );
 
-export default Footer;
+}
