dfbafe73eb4c92a859a9bce4efa91c2d0eaa9188
Rahul Sundaresan <s.rahul@protonmail.com>
2020-05-17 21:51:16 -0700

    fix messup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,14 @@
-export const siteMetadata = {
-  title: "Welcome to my personal website!",
-  siteUrl: `https://rsun.dev`,
-  description: `Rahul's personal website written in gatsby`
-};
-export const plugins = [
-  'gatsby-plugin-react-helmet',
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: config.manifestName,
-      short_name: config.manifestShortName,
-      start_url: config.pathPrefix || config.manifestStartUrl,
-      background_color: config.manifestBackgroundColor,
-      theme_color: config.manifestThemeColor,
-      display: config.manifestDisplay,
-      icon: config.manifestIcon,
-    },
+module.exports = {
+  
+  siteMetadata: {
+    title : `Welcome to my personal website!`,
+    siteUrl: `https://rsun.dev`,
+    description: `Rahul's personal website written in gatsby`
   },
-  'gatsby-plugin-sass',
-  'gatsby-plugin-offline',
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`
-];
+  plugins:[
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
+  ]
+};
