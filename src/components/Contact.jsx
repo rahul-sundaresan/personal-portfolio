04988c55153e0c8e11f59136c96220555c7d75ed
Rahul Sundaresan <s.rahul@protonmail.com>
2020-06-01 09:00:46 -0700

    add netlify form and clean up a bit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Contact(params) {
+export default function Contact() {
    return (
       
       <section id="contact">
@@ -11,16 +11,15 @@ export default function Contact(params) {
             </div>
          <div className="row">
                <div className="eight columns">
-                  <form method="post" id="contactForm" name="contactForm">
-                  <input type="hidden" name="form-name" value="contactForm" />
+                  <form method="post" id="contactForm" name="contactForm" data-netlify="true">
                   <fieldset>
                      <div>
-                        <label htmlFor="contactName">Name <span className="required">*</span></label>
+                        <label htmlFor="contactName">Name</label>
                         <input type="text" defaultValue="" size="35" id="contactName" name="contactName" />
                      </div>
 
                      <div>
-                        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                        <label htmlFor="contactEmail">Email</label>
                         <input type="email" defaultValue="" size="35" id="contactEmail" name="contactEmail" />
                      </div>
 
@@ -31,21 +30,13 @@ export default function Contact(params) {
 
                      <div>
                         <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                        <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                        <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"/>
                      </div>
                      <div>
                         <button type="submit" className="submit">Submit</button>
-                        <span id="image-loader">
-                           <img alt="" src="images/loader.gif" />
-                        </span>
                      </div>
                   </fieldset>
                   </form>
-
-            <div id="message-warning"> Error boy</div>
-                  <div id="message-success">
-                     <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-                  </div>
             </div>
          </div>
       </section>
