0bf127efc1d3ebdcb34426c672afc4f881a2745d
Rahul <rahul-sundaresan@users.noreply.github.com>
2024-10-13 17:02:05 -0400

    Temporary placeholder site

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer';
-import Resume from '../components/Resume/Resume';
-import Contact from '../components/Contact';
-import resumeData from '../../resumeData.json'
-import Navbar from "../components/Navbar/Navbar";
-import YAMLData from "../../content/resume-data.yaml"
+// import Header from '../components/Header/Header';
+// import Footer from '../components/Footer';
+// import Resume from '../components/Resume/Resume';
+// import Contact from '../components/Contact';
+// import resumeData from '../../resumeData.json'
+// import Navbar from "../components/Navbar/Navbar";
+// import YAMLData from "../../content/resume-data.yaml"
+// import YAMLData from "../../content/resume-data.yaml"
 
 export default function App() {
     return (
+        
         <>
-            <Navbar/>
+            <img src="catto.gif" alt="Cat waving at you"></img>
+        </>
+        {/*             <Navbar/>
             <Header name={YAMLData.name} tagline={YAMLData.tagline} personalInfo={YAMLData.personalinfo}/>
             <Contact/>
-            <Footer data={resumeData.main}/>
-        </>
+            <Footer data={resumeData.main}/> */}
+        
     );
 }
